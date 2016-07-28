212c8ec812ef1bfc35713b13b251680004b94830
silverroxy79 <vabro001@mail.goucher.edu>
2016-07-28 10:02:01 -0400

    Generative Art
    commit 2

--- sketch.js
+++ sketch.js
@@ -8,6 +8,7 @@ var y //position along the y-axis
 
 function setup() {
   createCanvas(1000,1000)
+  
   mic = new p5.AudioIn();
    mic.start();
    fft = new p5.FFT();
@@ -43,4 +44,47 @@ function setup() {
 
 function draw() {
   background(0)
+  
+  song.play()
+  
+  fill(255,0,0)
+  rect(bar[0],[0],[1],[2],[3])
+  rect(bar[1],[0],[1],[2],[3])
+  rect(bar[2],[0],[1],[2],[3])
+  rect(bar[3],[0],[1],[2],[3])
+  rect(bar[4],[0],[1],[2],[3])
+  rect(bar[5],[0],[1],[2],[3])
+  rect(bar[6],[0],[1],[2],[3])
+  rect(bar[7],[0],[1],[2],[3])
+  rect(bar[8],[0],[1],[2],[3])
+  rect(bar[9],[0],[1],[2],[3])
+  //rising bars
+  
+  fill(0,255,0)
+  ellipse(ball[0],[0],[1],[2],[3])
+  ellipse(ball[1],[0],[1],[2],[3])
+  ellipse(ball[2],[0],[1],[2],[3])
+  ellipse(ball[3],[0],[1],[2],[3])
+  ellipse(ball[4],[0],[1],[2],[3])
+  ellipse(ball[5],[0],[1],[2],[3])
+  ellipse(ball[6],[0],[1],[2],[3])
+  ellipse(ball[7],[0],[1],[2],[3])
+  ellipse(ball[8],[0],[1],[2],[3])
+  ellipse(ball[9],[0],[1],[2],[3])
+  //bouncing balls
+  
+  var spectrum = fft.analyze();
+
+   beginShape();
+   for (i = 0; i<spectrum.length; i++) {
+    rect(i, map(spectrum[i], 0, 25.5, height, 0) );
+   }
+   endShape();
+   
+   beginShape();
+   for (i = 0; i<spectrum.length; i++) {
+    ellipse(i, map(spectrum[i], 0, 25.5, height, 0) );
+   }
+   endShape();
+  
 }
