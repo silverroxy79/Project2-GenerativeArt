815a3498a3eeccb383cb3a07444a893bf8f96ac3
silverroxy79 <vabro001@mail.goucher.edu>
2016-07-29 16:03:33 -0400

    Project2
    Final Version?

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 var x //positon along the x-axis
 var y //position along the y-axis
+var pbary //previous position of bar along y
+var bary  //current position of bar along y
 var song
 
 function preload() {
@@ -16,8 +18,8 @@ function setup() {
    
   song.play()
    
-   bar=[
-     [0,950,100,50],
+  bar1=[
+     [0,950,100,50,],
      [100,950,100,50],
      [200,950,100,50],
      [300,950,100,50],
@@ -29,17 +31,43 @@ function setup() {
      [900,950,100,50],
      ]
      
-   ball=[
-     [50,900,100,100],
-     [150,900,100,100],
-     [250,900,100,100],
-     [350,900,100,100],
-     [450,900,100,100],
-     [550,900,100,100],
-     [650,900,100,100],
-     [750,900,100,100],
-     [850,900,100,100],
-     [950,900,100,100],
+  bar2=[
+     [0,0,100,50],
+     [100,0,100,50],
+     [200,0,100,50],
+     [300,0,100,50],
+     [400,0,100,50],
+     [500,0,100,50],
+     [600,0,100,50],
+     [700,0,100,50],
+     [800,0,100,50],
+     [900,0,100,50],
+     ]
+     
+  ball1=[
+     [950,50,100,100],
+     [950,150,100,100],
+     [950,250,100,100],
+     [950,350,100,100],
+     [950,450,100,100],
+     [950,550,100,100],
+     [950,650,100,100],
+     [950,750,100,100],
+     [950,850,100,100],
+     [950,950,100,100],
+     ]
+     
+  ball2=[
+     [50,50,100,100],
+     [50,150,100,100],
+     [50,250,100,100],
+     [50,350,100,100],
+     [50,450,100,100],
+     [50,550,100,100],
+     [50,650,100,100],
+     [50,750,100,100],
+     [50,850,100,100],
+     [50,950,100,100],
      ]
      
 //  spect=[100,30,45,60,75,90,105,120,135,150]
@@ -49,44 +77,24 @@ function draw() {
   background(0)
   
   var spectrum = freq.analyze();
-//  console.log(spectrum)
-//  console.log(spectrum[100])
-  
+
   for (i=0;i<10;i++) {
-    fill(255,0,0)
-    rect(bar[i][0],bar[i][1],bar[i][2],bar[i][3])
-  
-//  if (spectrum>0 && spectrum<2000) {
-    rect(bar[i][0],map(spectrum[i*100], 0, 150, height, 0),bar[i][2],map(spectrum[i*100], 0, 150, height, 0))
-//    rect(map(spectrum.getlevel(), 0, 1, height, 0),bar[i][1],map(spectrum.getlevel(), 0, 1, height, 0),bar[i][3])
+    fill(random(0,255),random(0,255),random(0,255),random(75,125))
+    rect(bar1[i][0],bar1[i][1],bar1[i][2],bar1[i][3])
+    fill(random(0,255),random(0,255),random(0,255),random(75,125))
+    rect(bar1[i][0],map(spectrum[i*100], 0, 150, height, 0),bar1[i][2],map(spectrum[i*100], 0, 150, height, 0))
+//rising bars 
+
+    fill(random(0,255),random(0,255),random(0,255),random(75,125))
+    rect(bar2[i][0],bar2[i][1],bar2[i][2],bar2[i][3])
+    fill(random(0,255),random(0,255),random(0,255),random(75,125))
+    rect(bar2[i][0],map(spectrum[i*100], 0, 150, 0, height),bar2[i][2],map(spectrum[i*100], 0, 150, 0, height))
+//falling bars  
+
+  fill(random(0,255),random(75,125))
+  ellipse(map(spectrum[i*100], 0, 150, width-50, 0),ball1[i][1],ball1[i][2],ball1[i][3])
+  fill(random(0,255),random(75,125))
+  ellipse(map(spectrum[i*100], 0, 150, 0, width-50),ball2[i][1],ball2[i][2],ball2[i][3])
+//bouncing balls    
   }
-  
-//  beginShape();
-//   for (i3 = 0; i3<spectrum.length; i3++) {
-//     fill(255,0,0)
-//     rect(map(spectrum[i3], 0, 25.5, height, 0),bar[i][1],bar[i][2],bar[i][3])
-//    vertex(i, map(spectrum[i], 0, 255, height, 0) );
-//   }
-//   endShape();
-  
-  //}
-  //rising bars
-  
-  for (i2=0;i2<10;i2++) {
-  fill(0,0,255)
-  ellipse(ball[i2][0],ball[i2][1],ball[i2][2],ball[i2][3])
-  
-  ellipse(ball[i2][0],map(spectrum[i2*100], 0, 150, height-50, 0),ball[i2][2],ball[i2][3])
-  
-//  beginShape();
-//   for (i4 = 0; i4<spectrum.length; i4++) {
-//     fill(0,0,255)
-//     ellipse(map(spectrum[i4], 0, 25.5, height, 0),ball[i2][1],ball[i2][2],ball[i2][3])
-//    vertex(i, map(spectrum[i], 0, 255, height, 0) );
-//   }
-//   endShape();
-  
-  }
-  //bouncing balls
-  
 }
